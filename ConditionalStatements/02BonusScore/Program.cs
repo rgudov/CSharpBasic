1e27fa76d50a477ca91de67c7055d083941bd061
Rado Gudov <rgudov@gmail.com>
2014-03-22 09:31:16 +0200

    HomeWork5

---
+++ ConditionalStatements/02BonusScore/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02BonusScore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write one number of interval 0...9 :");
+            int num = int.Parse(Console.ReadLine());
+            int score1 = num * 10;
+            int score2 = num * 100;
+            int score3 = num * 1000;
+
+            if (num > 0 && num < 4)
+            {
+                Console.WriteLine("Your score is: {0}", score1);
+
+            }
+            if (num > 3 && num < 7 )
+            {
+                Console.WriteLine("Your score is: {0}", score2);
+            }
+            if (num > 6 && num < 10)
+            {
+                Console.WriteLine("Your score is: {0}", score3);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input!");
+            }
+
+
+
+        }
+    }
+}
---
+++ ConditionalStatements/05TheBiggersOf3Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05TheBiggersOf3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a=");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("b=");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("c=");
+            double c = double.Parse(Console.ReadLine());
+            Console.WriteLine("The biggers is:");
+            if (a >= b && a >= c)
+            {
+                Console.WriteLine(a);
+            }
+            else if (b >= a && b >= c)
+            {
+                Console.WriteLine(b);
+            }
+            else if (c >= a && c >= b)
+            {
+                Console.WriteLine(c);
+            }
+            
+
+        }
+    }
+}
